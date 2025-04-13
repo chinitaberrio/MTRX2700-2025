c84873fe8d589e6c09f5610fed8f69ed3072357e
Stephany Berrio <30284346+chinitaberrio@users.noreply.github.com>
2025-04-14 09:55:05 +1000

    changing period for PWM

--- W08-HAL-test/Core/Src/main.c
+++ W08-HAL-test/Core/Src/main.c
@@ -287,7 +287,7 @@ static void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 47;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 1500;
+  htim2.Init.Period = 20000;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
